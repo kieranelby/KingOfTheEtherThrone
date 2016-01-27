f749f0eb99b13eb5f24b68ef3baa17dd1618e30a
Kieran Elby <kieran@dunelm.org.uk>
2016-01-27 21:40:36 +0000

    flesh out front-end structure slightly

---
+++ bin/build-public.js
@@ -0,0 +1,5 @@
+console.log("TODO - generate public content");
+console.log("TODO - talk to web.eth3 to get monarchy data");
+console.log("TODO - use template to generate html from monarchy data");
+console.log("TODO - use marked to generate html from README.md");
+console.log("TODO - copy files needed by public at run-time");
