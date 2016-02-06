dd5784369bb0b30d9b8d7923996a9c303862c9e5
Kieran Elby <kieran@dunelm.org.uk>
2016-02-06 15:36:54 +0000

    Autogenerated from build-public

--- public/templates.js
+++ public/templates.js
@@ -11,7 +11,7 @@ output += "Mood\">\n<form id=\"interfaceForm\" class=\"pure-form pure-form-align
 output += runtime.suppressValue(runtime.contextOrFrameLookup(context, frame, "statusText"), env.opts.autoescape);
 output += "</div>\n    <div class=\"pure-control-group\">\n      <label>&nbsp;</label>\n      <button id=\"refreshInterfaceButton\" class=\"pure-button\" onclick=\"KingOfTheEtherDapp.reloadClicked()\">Reload</button>\n    </div>\n  </fieldset>\n  <fieldset>\n    <legend>Become the Monarch</legend>\n    <div class=\"pure-control-group\">\n      <label for=\"currentClaimPriceInput\">Current Claim Price</label>\n      <input id=\"currentClaimPriceInput\" type=\"text\" value=\"";
 output += runtime.suppressValue(runtime.contextOrFrameLookup(context, frame, "currentClaimPrice"), env.opts.autoescape);
-output += "\" readonly>\n    </div>\n    <div class=\"pure-control-group\">\n      <label for=\"yourNameInput\">Your Regal Name</label>\n      <input id=\"yourNameInput\" type=\"text\" value=\"";
+output += "\" readonly>\n    </div>\n    <div class=\"pure-control-group\">\n      <label for=\"yourNameInput\">Your Regal Name</label>\n      <input id=\"yourNameInput\" type=\"text\" maxlength=\"20\" size=\"20\" value=\"";
 output += runtime.suppressValue(runtime.contextOrFrameLookup(context, frame, "yourName"), env.opts.autoescape);
 output += "\">\n    </div>\n    <div class=\"pure-control-group\">\n      <label>&nbsp;</label>\n      <button type=\"submit\" id=\"claimThroneButton\" class=\"pure-button pure-button-primary\" onclick=\"KingOfTheEtherDapp.claimThroneClicked()\">Claim Throne</button>\n    </div>\n  </fieldset>\n  <fieldset>\n    <legend>View Hall of Great Monarchs</legend>\n    <div class=\"pure-control-group\">\n      <label for=\"monarchNumberSelect\">Number</label>\n      <select id=\"monarchNumberSelect\" onchange=\"KingOfTheEtherDapp.selectedMonarchChanged()\">\n        <option value=\"NA\">-- Choose Monarch --</option>\n        ";
 frame = frame.push();
@@ -45,9 +45,9 @@ output += "\n      </select>\n    </div>\n    <div class=\"pure-control-group\">
 output += runtime.suppressValue(runtime.contextOrFrameLookup(context, frame, "hallMonarchFate"), env.opts.autoescape);
 output += "\" readonly>\n    </div>\n    <div class=\"pure-control-group\">\n      <label for=\"monarchName\">Name</label>\n      <input id=\"monarchName\" type=\"text\" value=\"";
 output += runtime.suppressValue(runtime.contextOrFrameLookup(context, frame, "hallMonarchName"), env.opts.autoescape);
-output += "\" readonly>\n    </div>\n    <div class=\"pure-control-group\">\n      <label for=\"monarchAddress\">Address</label>\n      <input id=\"monarchAddress\" type=\"text\" value=\"";
+output += "\" maxlength=\"20\" size=\"20\" readonly>\n    </div>\n    <div class=\"pure-control-group\">\n      <label for=\"monarchAddress\">Address</label>\n      <input id=\"monarchAddress\" type=\"text\" value=\"";
 output += runtime.suppressValue(runtime.contextOrFrameLookup(context, frame, "hallMonarchAddress"), env.opts.autoescape);
-output += "\" readonly>\n    </div>\n    <div class=\"pure-control-group\">\n      <label for=\"monarchClaimPrice\">Claim Price Paid</label>\n      <input id=\"monarchClaimPrice\" type=\"text\" value=\"";
+output += "\" maxlength=\"32\" size=\"32\" readonly>\n    </div>\n    <div class=\"pure-control-group\">\n      <label for=\"monarchClaimPrice\">Claim Price Paid</label>\n      <input id=\"monarchClaimPrice\" type=\"text\" value=\"";
 output += runtime.suppressValue(runtime.contextOrFrameLookup(context, frame, "hallMonarchClaimPrice"), env.opts.autoescape);
 output += "\" readonly>\n    </div>\n  </fieldset>\n</form>\n</div>";
 if(parentTemplate) {
