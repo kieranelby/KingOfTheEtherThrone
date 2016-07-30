d4f5d01ba778da949c26ab5125ae7da8eff5e08f
Kieran Elby <kieran@dunelm.org.uk>
2016-07-30 21:33:43 +0100

    provisioning etc

--- bin/refresh.sh
+++ bin/refresh.sh
@@ -3,8 +3,11 @@ set -euo pipefail
 
 cd /home/kgen/KingOfTheEtherThrone
 
+npm install
+
 while true; do 
     npm run chain-to-data http://localhost:8646 0xc41e3a6e381fe5d1331da38acf08cc9d48b111a8
     npm run data-to-website
-    rsync generated/ /var/www/kingoftheether/
+    rsync -r generated/ /var/www/kingoftheether/
+    sleep 30
 done
