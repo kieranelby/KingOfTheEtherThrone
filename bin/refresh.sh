aa29decb31e3f3c636698b3e199d65edc1cf6802
Kieran Elby <kieran@dunelm.org.uk>
2016-07-30 21:41:26 +0100

    fixes for content generation mostly

--- bin/refresh.sh
+++ bin/refresh.sh
@@ -8,6 +8,8 @@ npm install
 while true; do 
     npm run chain-to-data http://localhost:8646 0xc41e3a6e381fe5d1331da38acf08cc9d48b111a8
     npm run data-to-website
+    echo "Copying to webserver ..."
     rsync -r generated/ /var/www/kingoftheether/
+    echo "Sleeping ..."
     sleep 30
 done
